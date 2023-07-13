d8fdac8881d5baf4820b6bd0391f91e2b1c19bdc
gabriel-0710 <dhop.sc@gmail.com>
2023-07-13 20:07:28 -0300

    changes 07-13

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import './App.css';
+import React from 'react';
+import logoHome from './components/logo-Home.png';
 
-import logoHome from './components/logo-Home.png'
+ function App() {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">     
+   return (
+     <div className="App">
+       
+       <header className="App-header">     
+  
+       <img src={logoHome} alt="logo"/>
+  
+       <div className="inside-App-Header">
+         
+       <input type="text" id='idInput' placeholder="Search characters"/><br></br>
+       <button>Search</button>
+       </div> 
+       </header>
+     </div>
+   );
 
-      <img src={logoHome} alt="logo" /> 
-
-      <div className="inside-App-Header">
-        
-      <input type="text" id='idInput' placeholder="Search characters"/><br></br>
-      <button>Search</button>
-      </div> 
-
-      </header>
-
-      
-
-
-    </div>
-  );
-}
+ }
 
 export default App;
